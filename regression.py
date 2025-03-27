dac6be895faabdd14435172eec17f7b622658693
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-03-27 23:22:38 +0000

    Change graph

--- regression.py
+++ regression.py
@@ -8,12 +8,13 @@ class PolynomialRegression:
         self.x = np.linspace(-1, 1, num_samples)
         self.y = np.sin(self.x * self.sine_scale * np.pi)
         
-    def plot_graph(self, coefficients):
+    def plot_graph(self, coefficients, show_scatter=False):
         x_smooth = np.linspace(-1, 1, 1000)
         y_smooth = np.sin(x_smooth * self.sine_scale * np.pi)
         p = np.poly1d(coefficients[::-1])
 
         plt.plot(x_smooth, y_smooth)
-        plt.scatter(self.x, self.y)
-        plt.plot(x_smooth, p(x_smooth), color='purple')
+        if show_scatter:
+            plt.scatter(self.x, self.y)
+        plt.plot(x_smooth, p(x_smooth), color='red')
         plt.show()
