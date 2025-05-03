5e5d6530344d957038f40edbfe8d678df832ed0c
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-03 16:22:41 +0100

    Arrange in columns and add markdown cell

--- notebooks/output-functions.py
+++ notebooks/output-functions.py
@@ -2,12 +2,30 @@ import marimo
 
 __generated_with = "0.13.4"
 app = marimo.App(
-    width="medium",
+    width="columns",
     layout_file="layouts/output-functions.grid.json",
 )
 
 
+@app.cell(column=0)
+def _():
+    import marimo as mo
+    return (mo,)
+
+
 @app.cell
+def _():
+    import numpy as np
+    return (np,)
+
+
+@app.cell
+def _():
+    import matplotlib.pyplot as plt
+    return (plt,)
+
+
+@app.cell(column=1)
 def _():
     max_val = 5
     return (max_val,)
@@ -29,12 +47,21 @@ def _(mo, sliders):
     return
 
 
-@app.cell
+@app.cell(column=2)
 def _(max_val, plt, sliders):
     plt.ylim((-max_val, max_val))
     plt.axhline(y=0, color='black', alpha=0.4, linewidth=1)
     plt.bar([f"a{i}" for i in range(len(sliders))],
             [slider.value for slider in sliders]
+           )
+    return
+
+
+@app.cell
+def _(plt, slider_values, sliders, softmax):
+    plt.ylim((0, 1))
+    plt.bar([f"a{i}" for i in range(len(sliders))],
+            softmax(slider_values)
            )
     return
 
@@ -50,28 +77,24 @@ def _(plt, sigmoid, slider_values, sliders):
     return
 
 
-@app.cell
-def _(plt, slider_values, sliders, softmax):
-    plt.ylim((0, 1))
-    plt.bar([f"a{i}" for i in range(len(sliders))],
-            softmax(slider_values)
-           )
+@app.cell(column=3)
+def _(mo):
+    mo.md(
+        r"""
+    ## Activations
+
+    This shows the raw activations of the ouput layer (the slider values) before any output/activation function has been applied.
+    """
+    )
     return
 
 
-@app.cell
+@app.cell(column=4)
 def _(np):
     def softmax(x):
         exp_x = np.exp(x - np.max(x))  # Subtract max for numerical stability
         return exp_x / np.sum(exp_x)
     return (softmax,)
-
-
-@app.cell
-def _(np):
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
-    return (sigmoid,)
 
 
 @app.cell
@@ -81,21 +104,10 @@ def _(np, sliders):
 
 
 @app.cell
-def _():
-    import numpy as np
-    return (np,)
-
-
-@app.cell
-def _():
-    import marimo as mo
-    return (mo,)
-
-
-@app.cell
-def _():
-    import matplotlib.pyplot as plt
-    return (plt,)
+def _(np):
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+    return (sigmoid,)
 
 
 if __name__ == "__main__":
