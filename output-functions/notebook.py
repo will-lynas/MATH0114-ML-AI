3ea9d42fcab0760eb8bfd88020554a596fdc1b32
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-03 15:41:04 +0100

    Add more sliders

--- output-functions/notebook.py
+++ output-functions/notebook.py
@@ -6,26 +6,45 @@ app = marimo.App(width="medium", layout_file="layouts/notebook.grid.json")
 
 @app.cell
 def _():
-    import marimo as mo
-    return (mo,)
-
-
-@app.cell
-def _(mo):
-    slider = mo.ui.slider(1,9)
-    slider
-    return (slider,)
-
-
-@app.cell
-def _():
     import matplotlib.pyplot as plt
     return (plt,)
 
 
 @app.cell
-def _(plt, slider):
-    plt.bar([1,2,3], [slider.value, 2, 4])
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _():
+    max_val = 5
+    return (max_val,)
+
+
+@app.cell
+def _(max_val, mo):
+    start_values = [1,5,2,3]
+    sliders = mo.ui.array([
+        mo.ui.slider(0, max_val, label=f"a{i}", value=value)
+        for i,value in enumerate(start_values)
+    ])
+    return (sliders,)
+
+
+@app.cell
+def _(mo, sliders):
+    mo.vstack(sliders)
+
+    return
+
+
+@app.cell
+def _(max_val, plt, sliders):
+    plt.ylim(top=max_val)
+    plt.bar([f"a{i}" for i in range(len(sliders))],
+            [slider.value for slider in sliders]
+           )
     return
 
 
