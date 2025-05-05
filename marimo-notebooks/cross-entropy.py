a4eb37897a200d7e5f2c60bcb4e5b45431d06462
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-05 21:32:19 +0100

    Add cross entropy skeleton

---
+++ marimo-notebooks/cross-entropy.py
@@ -0,0 +1,22 @@
+import marimo
+
+__generated_with = "0.13.4"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import numpy as np
+    import altair as alt
+    return
+
+
+@app.cell
+def _():
+    test = 42
+    return
+
+
+if __name__ == "__main__":
+    app.run()
