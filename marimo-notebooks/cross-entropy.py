47360badf1ff75ffd9ecf74ca9626e58cc54946e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-05 21:59:14 +0100

    Add proper cross entropy

--- marimo-notebooks/cross-entropy.py
+++ marimo-notebooks/cross-entropy.py
@@ -9,12 +9,57 @@ def _():
     import marimo as mo
     import numpy as np
     import altair as alt
+    return alt, mo, np
+
+
+@app.cell
+def _(mo):
+    mo.md("""
+          ## Cross Entropy:
+
+          $$
+          t \\ln(y) + (1-t)\\ln(1-y)
+          $$
+          """)
     return
 
 
 @app.cell
-def _():
-    test = 42
+def _(np):
+    def cross_entropy(t, y):
+        return - (t * np.log(y) + (1 - t) * np.log(1 - y))
+    return (cross_entropy,)
+
+
+@app.cell
+def _(mo):
+    slider = mo.ui.slider(0.01, 0.99, step=0.01, value=0.5, label="y")
+    slider
+    return (slider,)
+
+
+@app.cell
+def _(cross_entropy, slider):
+    y_value = slider.value
+    ce_t0 = cross_entropy(0, y_value)
+    ce_t1 = cross_entropy(1, y_value)
+    return ce_t0, ce_t1
+
+
+@app.cell
+def _(alt, ce_t0, ce_t1):
+    source = alt.Data(values=[
+        {"t": "t=0", "cross_entropy": ce_t0},
+        {"t": "t=1", "cross_entropy": ce_t1}
+    ])
+
+    chart = alt.Chart(source).mark_bar().encode(
+        x='t:N',
+        y=alt.Y('cross_entropy:Q', scale=alt.Scale(domain=(0, 5))),
+        color='t:N'
+    ).properties(title=f"Cross Entropy")
+
+    chart
     return
 
 
