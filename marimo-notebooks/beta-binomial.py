b40a085490bcbf9ef1b615b8b90a5e43ec2d8eb4
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-06 10:20:02 +0100

    Add ev of posterior

--- marimo-notebooks/beta-binomial.py
+++ marimo-notebooks/beta-binomial.py
@@ -10,24 +10,25 @@ def _():
     import numpy as np
     import altair as alt
     import scipy.stats as stats
-    return mo, np, alt, stats
+    return alt, mo, np, stats
+
 
 @app.cell
 def _(mo):
     mo.md(r"""
          Suppose we are modeling some Binomial process. The probability of success $p$ is unknown to us. It is a latent variable.
          We wish to estimate $p$.
-         
+
          At this stage, we have observed no data, but from our knowledge of the process, we have a prior belief about the value of $p$.
-        
+
          Let's say we believe $p$ is distributed according to a Beta distribution with parameters $\alpha$ and $\beta$. That is
-         
+
          $$
          p \sim \text{Beta}(\alpha, \beta)
          $$
-         
-          """)
 
+          """)
+    return
 
 
 @app.cell
@@ -39,7 +40,7 @@ def _(mo):
 
 
 @app.cell
-def _(mo, alt, alpha_slider, beta_slider, np, stats):
+def _(alpha_slider, alt, beta_slider, mo, np, stats):
     def prior_chart():
         x = np.linspace(0, 1, 1000)
         pdf = stats.beta.pdf(x, alpha_slider.value, beta_slider.value)
@@ -56,72 +57,80 @@ def _(mo, alt, alpha_slider, beta_slider, np, stats):
             title=f"Beta Distribution - Prior",
             config=alt.Config(axis=alt.AxisConfig(grid=False))
         )
-        
+
         return chart
 
     mo.ui.altair_chart(prior_chart())
     return
 
+
 @app.cell
-def _(mo, alpha_slider, beta_slider):
+def _(alpha_slider, beta_slider, mo):
     mo.md(f"""
         The **Expected Value** of the Beta distribution is given by
-         
+
         $$
          \\mathbb{{E}}[p] = \\frac{{\\alpha}}{{\\alpha + \\beta}}
           = \\frac{{{alpha_slider.value}}}{{{alpha_slider.value} + {beta_slider.value}}}
           = {alpha_slider.value / (alpha_slider.value + beta_slider.value):.3f}
         $$
-        
+
         So this represents our single point estimate of $p$ before we have observed any data.
          """)
-    
+    return
+
+
 @app.cell
 def _(mo):
     mo.md(r"""
         Now, let's observe some data by doing a Binomial experiment with $n$ trials.
         """)
+    return
 
 
 @app.cell
 def _(mo):
     n_slider = mo.ui.slider(1, 100, step=1, value=10, label="n")
     n_slider
-    return n_slider
+    return (n_slider,)
+
 
 @app.cell
-def _(mo, n_slider, alpha_slider, beta_slider):
+def _(alpha_slider, beta_slider, mo, n_slider):
     mean_p = alpha_slider.value / (alpha_slider.value + beta_slider.value)
     mo.md(f"""
         With  $n = {n_slider.value}$ trials, we expect to see
         $n * \\mathbb{{E}}[p] = {n_slider.value} * {mean_p:.3f} = {n_slider.value * mean_p:.1f}$ successes, based on our prior belief.
         """)
+    return
+
 
 @app.cell
 def _(mo, n_slider):
     k_slider = mo.ui.slider(0, n_slider.value, step=1, value=0, label="k")
     k_slider
-    return k_slider
+    return (k_slider,)
 
 
 @app.cell
-def _(mo, n_slider, k_slider):
+def _(k_slider, mo):
     mo.md(f"""
         What if, instead, we observe $k = {k_slider.value}$ successes?
-        
+
         Well, we can use Bayes' theorem to update our **prior** belief about $p$, using the observed data, to obtain a **posterior** belief.
-        
+
         In the case of a $Beta(\\alpha, \\beta)$ prior, with a Binomial likelihood, the posterior is also a Beta distribution:
-        
+
         $$
         p \\sim \\text{{Beta}}(\\alpha + k, \\beta + n - k)
         $$
         """
         )
+    return
 
 
 @app.cell
-def _(mo, alt, alpha_slider, beta_slider, np, stats, n_slider, k_slider):
+def _(alpha_slider, alt, beta_slider, k_slider, mo, n_slider, np, stats):
     def posterior_chart():
         x = np.linspace(0, 1, 1000)
         pdf_posterior = stats.beta.pdf(x, alpha_slider.value + k_slider.value, beta_slider.value + n_slider.value - k_slider.value)
@@ -146,6 +155,21 @@ def _(mo, alt, alpha_slider, beta_slider, np, stats, n_slider, k_slider):
         return chart
 
     mo.ui.altair_chart(posterior_chart())
+    return
+
+
+@app.cell
+def _(alpha_slider, beta_slider, k_slider, mo, n_slider):
+    mo.md(f"""
+        After seeing the data, the new **Expected Value** of $p$ is given by
+
+        $$
+         \\mathbb{{E}}[p] = \\frac{{\\alpha + k}}{{\\alpha + \\beta + n - k}}
+          = \\frac{{{alpha_slider.value} + {k_slider.value}}}{{{alpha_slider.value} + {beta_slider.value} + {n_slider.value} - {k_slider.value}}}
+          = {((alpha_slider.value + k_slider.value) / (alpha_slider.value + beta_slider.value + n_slider.value - k_slider.value)):.3f}
+        $$
+         """)
+    return
 
 
 if __name__ == "__main__":
