4af21250577df0b30e2cb53b84c27de341d33751
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-06 09:48:32 +0100

    Add second chart

--- marimo-notebooks/beta-binomial.py
+++ marimo-notebooks/beta-binomial.py
@@ -23,33 +23,69 @@ def _(mo):
 
 @app.cell
 def _(mo, alt, alpha_slider, beta_slider, np, stats):
-    x = np.linspace(0, 1, 1000)
-    pdf = stats.beta.pdf(x, alpha_slider.value, beta_slider.value)
+    def prior_chart():
+        x = np.linspace(0, 1, 1000)
+        pdf = stats.beta.pdf(x, alpha_slider.value, beta_slider.value)
 
-    source = alt.Data(values=[
-        {"x": float(x_val), "pdf": float(pdf_val)} 
-        for x_val, pdf_val in zip(x, pdf)
-    ])
+        source = alt.Data(values=[
+            {"x": float(x_val), "pdf": float(pdf_val)} 
+            for x_val, pdf_val in zip(x, pdf)
+        ])
 
-    chart = alt.Chart(source).mark_line().encode(
-        x='x:Q',
-        y=alt.Y('pdf:Q', scale=alt.Scale(domain=[0, 6])),
-    ).properties(
-        title=f"Beta Distribution",
-        config=alt.Config(axis=alt.AxisConfig(grid=False))
-    )
-    
-    mo.ui.altair_chart(chart)
+        chart = alt.Chart(source).mark_line().encode(
+            x='x:Q',
+            y=alt.Y('pdf:Q', scale=alt.Scale(domain=[0, 6])),
+        ).properties(
+            title=f"Beta Distribution",
+            config=alt.Config(axis=alt.AxisConfig(grid=False))
+        )
+        
+        return chart
+
+    mo.ui.altair_chart(prior_chart())
     return
 
 @app.cell
 def _(mo):
-    n_slider = mo.ui.slider(0, 100, step=1, value=10, label="n")
-    k_slider = mo.ui.slider(0, 100, step=1, value=5, label="k")
-    mo.vstack([n_slider, k_slider])
-    return n_slider, k_slider
+    n_slider = mo.ui.slider(1, 100, step=1, value=10, label="n")
+    n_slider
+    return n_slider
+
+@app.cell
+def _(mo, n_slider):
+    k_slider = mo.ui.slider(0, n_slider.value, step=1, value=0, label="k")
+    k_slider
+    return k_slider
 
 
+
+
+@app.cell
+def _(mo, alt, alpha_slider, beta_slider, np, stats, n_slider, k_slider):
+    def posterior_chart():
+        x = np.linspace(0, 1, 1000)
+        pdf_posterior = stats.beta.pdf(x, alpha_slider.value + k_slider.value, beta_slider.value + n_slider.value - k_slider.value)
+        pdf_prior = stats.beta.pdf(x, alpha_slider.value, beta_slider.value)
+
+        source = alt.Data(values=[
+            {"x": float(x_val), "type": "Posterior", "pdf": float(pdf_posterior_val)}
+            for x_val, pdf_posterior_val in zip(x, pdf_posterior)
+        ] + [
+            {"x": float(x_val), "type": "Prior", "pdf": float(pdf_prior_val)}
+            for x_val, pdf_prior_val in zip(x, pdf_prior)
+        ])
+
+        chart = alt.Chart(source).mark_line().encode(
+            x='x:Q',
+            y=alt.Y('pdf:Q', scale=alt.Scale(domain=[0, 24])),
+            color=alt.Color('type:N', scale=alt.Scale(domain=['Prior', 'Posterior'], range=['#1f77b4', 'orange']))
+        ).properties(
+            title=f"Beta Distribution - Prior and Posterior",
+            config=alt.Config(axis=alt.AxisConfig(grid=False))
+        )
+        return chart
+
+    mo.ui.altair_chart(posterior_chart())
 
 
 if __name__ == "__main__":
